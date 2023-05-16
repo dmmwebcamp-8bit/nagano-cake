a86dfca115ba0b1d889f14eda53587cd06861b53
yuki-nishimoto1109 <126940102+yuki-nishimoto1109@users.noreply.github.com>
2023-05-16 11:52:53 +0000

    [Finish] header

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,8 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
+  def full_name
+    last_name + " " + first_name
+  end
+
 end
