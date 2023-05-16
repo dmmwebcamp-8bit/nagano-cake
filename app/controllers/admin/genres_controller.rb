3df0e1b87b0f906f2e9049c6dda849ceead76b1e
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-16 16:45:03 +0000

    [Update] admin/genres配下のコントローラ/ビュー内への記述

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
