105d8dc3231c6844a8c23969ca79da26abcee3c0
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-17 06:43:07 +0000

    [Change] ページネーション表示形式の変更

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -5,8 +5,8 @@ Kaminari.configure do |config|
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
-  # config.left = 0
-  # config.right = 0
+  config.left = 2
+  config.right = 1
   # config.page_method_name = :page
   # config.param_name = :page
   # config.max_pages = nil
