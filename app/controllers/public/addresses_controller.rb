9a2cad6658243e9f653b3997785e2c8e291e4387
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-17 13:49:38 +0000

    [Update] public/addresses配下のVCへの記述

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,37 @@
 class Public::AddressesController < ApplicationController
+
   def index
+    @address = Address.new
+    @customer = current_customer
+    @addresses = @customer.addresses
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to request.referer
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end
