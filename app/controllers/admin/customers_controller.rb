9ff1fbf0b6cf53f3a0454617c65d526a38e2e3df
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-17 07:25:00 +0000

    [Add]会員一覧/会員詳細/会員情報編集/更新機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,27 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all.order(id: "ASC").page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    flash[:notice] = "会員情報を更新しました"
+    redirect_to admin_customers_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :postal_code, :address, :is_deleated)
+  end
+
 end
