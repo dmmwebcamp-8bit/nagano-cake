1b52982543a257092b2834b5a02c541ce193e8b2
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-18 13:45:13 +0000

    [Update] public/itemsの作成に伴うpublic/cart_items配下のMVCへの追記

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -23,20 +23,17 @@ class Public::CartItemsController < ApplicationController
 
   def create
     cart_item = current_customer.cart_items.new(cart_item_params)
+    cart_item.item_id = cart_item_params[:item_id]
     if cart_item.save
       redirect_to cart_items_path
     else
-      render request.referer
+      redirect_to request.referer
     end
   end
 
   private
     def cart_item_params
-      params.require(:care_item).permit(:item_id, :amount)
-    end
-
-    def subtotal
-        item.with_tax_price * amount
+      params.require(:cart_item).permit(:item_id, :amount)
     end
 
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,8 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
+  def subtotal
+     ((item.price * amount) * 1.1).round
+  end
+
 end
