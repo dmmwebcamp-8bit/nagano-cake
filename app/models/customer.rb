e749a6992787b885c3ad2b7f80d8a9d371e1c285
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-18 05:33:15 +0000

    [Add]public/customerモデルへバリデーション追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,14 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :phone_number, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+
   def full_name
     last_name + first_name
   end
