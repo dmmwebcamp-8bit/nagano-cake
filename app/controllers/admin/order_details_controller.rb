5d450cde4bc5b95c774129db74ccfa0a531914f1
yuki-nishimoto1109 <126940102+yuki-nishimoto1109@users.noreply.github.com>
2023-05-18 01:58:57 +0000

    [Finish] admin/order

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     order_detail = OrderDetail.find(params[:id])
-    order_detail.update(making_status: params[:making_status])
+    order_detail.update(making_status: params[:order_detail][:making_status])
     redirect_to admin_order_path(params[:order_id])
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,10 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def update
+    order = Order.find(params[:id])
+    order.update(status: params[:order][:status])
+    redirect_to admin_order_path(order.id)
+  end
 end
