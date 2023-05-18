9319938557c57f323a2a9c96216512319cdb0b08
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-18 05:23:12 +0000

    [Update]アクセス制限

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CustomersController < ApplicationController
+    before_action :authenticate_admin!
+
   def index
     @customers = Customer.page(params[:page]).per(10)
   end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,6 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
   def show
   end
 
