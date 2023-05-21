4dd6e8fec3732d466dbc1620e0367477e8417184
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-21 05:39:12 +0000

    [Add]バリデーション

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -87,7 +87,6 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :postal_code, :address])
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, :postal_code, :address])
   end
 
   def customer_params
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,13 +8,38 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :phone_number, presence: true
-  validates :postal_code, presence: true
-  validates :address, presence: true
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :address
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。'} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+    
+    with_options numericality: {only_integer: true} do
+      validates :phone_number
+      validates :postal_code, length: { is: 7, message: 'は7桁で入力して下さい。' }
+    end
+  end
+
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :first_name_kana,
+  #           presence: true,
+  #           format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。'}
+  # validates :last_name_kana,
+  #           presence: true,
+  #           format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。'}
+  # validates :phone_number,
+  #           presence: true,
+  #           numericality: {only_integer: true}
+  # validates :postal_code,
+  #           presence: true,
+  #           numericality: { only_integer: true},
+  #           length: { is: 7, message: 'は7桁で入力して下さい。' }
+  # validates :address, presence: true
 
   def full_name
     last_name + first_name
