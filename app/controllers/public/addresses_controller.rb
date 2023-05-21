072d7dd814d533a0e0e71a5e958ca250ceac559e
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-21 15:07:51 +0000

    [Update] 配送先登録に関連するバリデーションの追加、エラーメッセージの設定

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -9,8 +9,12 @@ class Public::AddressesController < ApplicationController
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
-    @address.save
-    redirect_to request.referer
+    if @address.save
+      redirect_to request.referer
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
@@ -19,8 +23,11 @@ class Public::AddressesController < ApplicationController
 
   def update
     @address = Address.find(params[:id])
-    @address.update(address_params)
-    redirect_to addresses_path
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
