13a94f2133a6b699e95262f7c0d3d5ea846726ac
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-23 04:30:36 +0000

    [Update]Public::ApplicationControllerへskip_before_action追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
 end
+
+# class Public::ApplicationController < ApplicationController
+#   before_action :authenticate_customer!, except: [:top,:about]
+# end
+
+# class Admin::ApplicationController < ApplicationController
+#   before_action :authenticate_admin!
+# end
--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,3 +1,11 @@
 class Public::ApplicationController < ApplicationController
   before_action :authenticate_customer!
 end
+
+class Public::ItemsController < Public::ApplicationController
+  skip_before_action :authenticate_customer!
+end
+
+class Public::HomesController < Public::ApplicationController
+  skip_before_action :authenticate_customer!
+end
