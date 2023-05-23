fd8d04fe08f7bf41e91a7ad28f909b2144d5668e
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-23 04:35:22 +0000

    [Remove]application_controller内の不要な記述削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
 end
-
-# class Public::ApplicationController < ApplicationController
-#   before_action :authenticate_customer!, except: [:top,:about]
-# end
-
-# class Admin::ApplicationController < ApplicationController
-#   before_action :authenticate_admin!
-# end
